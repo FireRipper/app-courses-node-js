fb47cf350d04e5875a6faa012a18ad3564abede1
FireRipper <shabelnyk.ilya@gmail.com>
2020-04-13 16:36:47 +0300

    feat(form add course): add form on page 'add' with some fields (name course, price, url-img), add route for process post request to '/add'.

--- routes/add.js
+++ routes/add.js
@@ -1,10 +1,17 @@
 const { Router } = require('express')
-
 const router = Router()
 
-module.exports = router.get('/', (req, res) => {
-    res.render('add',  {
+router.get('/', (req, res) => {
+    res.render('add', {
         title: 'Добавить курс',
         isAdd: true
     })
 })
+
+router.post('/', (req, res) => {
+    console.log(req.body)
+
+    res.redirect('/courses')
+})
+
+module.exports = router
