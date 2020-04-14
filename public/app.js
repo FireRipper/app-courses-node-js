d48e89c09dd10a96c90c1fca5400d872ca0eea1c
FireRipper <shabelnyk.ilya@gmail.com>
2020-04-14 16:32:28 +0300

    feat(view card): add view for correct display courses or display empty basket. Change one line in index.js.

--- public/app.js
+++ public/app.js
@@ -1,6 +1,52 @@
-document.querySelectorAll('.price').forEach(node => {
-    node.textContent = new Intl.NumberFormat('ru-Ru', {
+// Variables
+const $card = document.querySelector('#card')
+const price = document.querySelectorAll('.price')
+
+// Functions
+/**
+ * To currency. Convert price to valid view.
+ * @param price
+ * @return {string}
+ */
+const toCurrency = price => {
+    return new Intl.NumberFormat('ru-Ru', {
         currency: 'rub',
         style: 'currency'
-    }).format(node.textContent)
+    }).format(price)
+}
+
+price.forEach(node => {
+    node.textContent = toCurrency(node.textContent)
 })
+
+// Events
+if ($card) {
+    $card.addEventListener('click', e => {
+        if (e.target.classList.contains('js-remove')) {
+            const id = e.target.dataset.id
+
+            fetch(`/card/remove/${id}`, {
+                method: 'delete'
+            }).then(res => res.json())
+                .then(card => {
+                    if (card.courses.length) {
+                        const html = card.courses.map(c => {
+                            return `
+                            <tr>
+                              <td>${c.title}</td>
+                              <td>${c.count}</td>
+                              <td>
+                                <button class="btn btn-small js-remove" data-id=${c.id}>Удалить</button>
+                              </td>
+                            </tr>
+                            `
+                        }).join('')
+                        $card.querySelector('tbody').innerHTML = html
+                        $card.querySelector('.price').textContent = toCurrency(card.price)
+                    } else {
+                        $card.innerHTML = `<p>Корзина пуста</p>`
+                    }
+                })
+        }
+    })
+}
