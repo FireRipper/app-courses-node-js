671f64bf16a11ff74f9e68ffa63bd4a8f5c9c44f
FireRipper <shabelnyk.ilya@gmail.com>
2020-04-14 13:20:40 +0300

    feat(model course): add folder data with file courses.json, add model course - process data from form, convert data to JSON and save to file courses.json. Using model in add.js (post request).

--- routes/courses.js
+++ routes/courses.js
@@ -2,9 +2,12 @@ const { Router } = require('express')
 
 const router = Router()
 
-module.exports = router.get('/', (req, res) => {
+router.get('/', (req, res) => {
     res.render('all-courses',  {
         title: 'Курсы',
         isCourses: true
     })
 })
+
+
+module.exports = router
--- routes/home.js
+++ routes/home.js
@@ -2,9 +2,11 @@ const { Router } = require('express')
 
 const router = Router()
 
-module.exports = router.get('/', (req, res) => {
+router.get('/', (req, res) => {
     res.render('index', {
         title: 'Главная страница',
         isHome: true
     })
 })
+
+module.exports = router
