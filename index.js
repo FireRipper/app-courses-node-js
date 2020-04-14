554b97540b8a70fe0c6a17bfc76b758ac16af05b
FireRipper <shabelnyk.ilya@gmail.com>
2020-04-14 15:24:04 +0300

    feat(add course to card): add router - card.js with some methods (get and post). Use router in index.js. Add link ('Корзина') to navbar.

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const exphbs = require('express-handlebars')
 const homeRoutes = require('./routes/home')
 const coursesRoutes = require('./routes/courses')
 const addRoutes = require('./routes/add')
+const cardRoutes = require('./routes/card')
 const app = express()
 
 const hbs = exphbs.create({
@@ -20,7 +21,7 @@ app.use(express.urlencoded({ extended: true }))
 app.use('/', homeRoutes)
 app.use('/courses', coursesRoutes)
 app.use('/add', addRoutes)
-
+app.use('/card', cardRoutes)
 
 const PORT = process.env.PORT || 5000
 
---
+++ models/card.js
@@ -0,0 +1,61 @@
+const path = require('path')
+const fs = require('fs')
+
+const p = path.join(
+    path.dirname(process.mainModule.filename),
+    'data',
+    'card.json'
+)
+
+class Card {
+
+    /**
+     * Add. Add course to card. If this course is in card (True: just update count in course; False: add new course to card)
+     * @param {object} course
+     * @return {Promise}
+     */
+    static async add(course) {
+        const card = await Card.fetch()
+
+        const idx = card.courses.findIndex(c => c.id === course.id)
+        const candidate = card.courses[idx]
+
+        if (candidate) {
+            candidate.count++
+            card.courses[idx] = candidate
+        } else {
+            course.count = 1
+            card.courses.push(course)
+        }
+
+        card.price += +course.price
+
+        return new Promise(((resolve, reject) => {
+            fs.writeFile(p, JSON.stringify(card), err => {
+                if(err) {
+                    reject(err)
+                } else {
+                    resolve()
+                }
+            })
+        }))
+    }
+
+    /**
+     * fetch. Get data from card.json
+     * @return {Promise<unknown>}
+     */
+    static async fetch() {
+        return new Promise(((resolve, reject) => {
+            fs.readFile(p, 'utf-8', (err, content) => {
+                if (err) {
+                    reject(err)
+                } else {
+                    resolve(JSON.parse(content))
+                }
+            })
+        }))
+    }
+}
+
+module.exports = Card
