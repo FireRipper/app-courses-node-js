f2bd3bfad1e4007e804872a027c5187d076d65df
FireRipper <shabelnyk.ilya@gmail.com>
2020-04-14 14:29:24 +0300

    feat(edit course): add new views - course (display information about course by id), course-edit (include form with data about course), add to routes some methods - get and post to courses.js. Add styles for course.js. Add two methods to model 'course' - getById (get course by id) and update.

--- models/course.js
+++ models/course.js
@@ -19,6 +19,38 @@ class Course {
         }
     }
 
+    /**
+     * Update. Get 1 course by id from courses.json and update them fields.
+     * @param course
+     * @return {Promise<unknown>}
+     */
+
+    static async update(course) {
+        const courses = await Course.getAll()
+
+        const idx = courses.findIndex(c => c.id === course.id)
+        courses[idx] = course
+
+        return new Promise(((resolve, reject) => {
+            fs.writeFile(
+                path.join(__dirname, '..', 'data', 'courses.json'),
+                JSON.stringify(courses),
+                (err) => {
+                    if (err) {
+                        reject(err)
+                    } else {
+                        resolve()
+                    }
+                }
+            )
+        }))
+    }
+
+    /**
+     * Save. Get all data from courses.json, push object (our course) to array and write him in courses.json
+     * @return {Promise<unknown>}
+     */
+
     async save() {
         const courses = await Course.getAll()
         courses.push(this.toJSON())
@@ -38,6 +70,10 @@ class Course {
         }))
     }
 
+    /**
+     * GetAll. Get all courses from courses.json
+     * @return {Promise<unknown>}
+     */
     static getAll() {
         return new Promise((resolve, reject) => {
             fs.readFile(
@@ -52,6 +88,16 @@ class Course {
                 }
             )
         })
+    }
+
+    /**
+     * getById. Get course by id from courses.json
+     * @param id
+     * @return {Promise<*|number|bigint|T|T>}
+     */
+    static async getById(id) {
+        const courses = await Course.getAll()
+        return courses.find(c => c.id === id)
     }
 }
 
--- routes/courses.js
+++ routes/courses.js
@@ -4,12 +4,39 @@ const router = Router()
 
 router.get('/', async (req, res) => {
     const courses = await Course.getAll()
-    res.render('all-courses',  {
+    res.render('all-courses', {
         title: 'Курсы',
         isCourses: true,
         courses
     })
 })
 
+router.get('/:id/edit', async (req, res) => {
+        if (!req.query.allow) {
+            return res.redirect('/')
+        }
+
+        const course = await Course.getById(req.params.id)
+
+        res.render('course-edit', {
+            title: `Редактировать ${course.title}`,
+            course
+        })
+    }
+)
+
+router.post('/edit', async (req, res) => {
+    await Course.update(req.body)
+    res.redirect('/courses')
+})
+
+router.get('/:id', async (req, res) => {
+    const course = await Course.getById(req.params.id)
+    res.render('course', {
+        layout: 'empty',
+        title: `Курс ${course.title}`,
+        course
+    })
+})
 
 module.exports = router
