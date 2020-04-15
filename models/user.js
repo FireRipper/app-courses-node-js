5a12436cb2a1cb6bb67493d36bc984a2f7eedeb8
FireRipper <shabelnyk.ilya@gmail.com>
2020-04-15 14:38:52 +0300

    refactor(model User): add new Scheme uses to models/user.js, create relationship between models 'User' and 'Course' for future display cart with 'Courses' which user choice.

---
+++ models/user.js
@@ -0,0 +1,30 @@
+const { Schema, model } = require('mongoose')
+
+const userSchema = new Schema({
+    email: {
+        type: String,
+        required: true
+    },
+    name: {
+        type: String,
+        required: true
+    },
+    cart: {
+        items: [
+            {
+                count: {
+                    type: Number,
+                    required: true,
+                    default: 1
+                },
+                courseId: {
+                    type: Schema.Types.ObjectId,
+                    ref: 'Course', // Reference with table (model) 'Course' - relationship !!!!
+                    required: true,
+                }
+            }
+        ]
+    }
+})
+
+module.exports = userSchema('User', userSchema)
