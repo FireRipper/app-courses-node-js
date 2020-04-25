2c5e2328597ab1354c50cc6dacf8accd04ca89ef
illia <shabelnyk.ilya@gmail.com>
2020-04-25 14:31:05 +0300

    feat(save session): add package connect-mongodb-session for save session in DB; change access to buttons on page all-courses for register (has access to buttons 'edit course' and 'buy course') and unregister (has access to only one button it's view information about 1 course) users.

--- index.js
+++ index.js
@@ -2,21 +2,31 @@ const express = require('express')
 const exphbs = require('express-handlebars')
 const mongoose = require('mongoose')
 const path = require('path')
+
 const session = require('express-session')
+const MongoStore = require('connect-mongodb-session')(session)
+
 const homeRoutes = require('./routes/home')
 const coursesRoutes = require('./routes/courses')
 const addRoutes = require('./routes/add')
 const cardRoutes = require('./routes/card')
 const ordersRoutes = require('./routes/orders')
 const authRoutes = require('./routes/auth')
-const User = require('./models/user')
+
 const varMiddleware = require('./middleware/variables')
+
+const MONGODB_URL = `mongodb+srv://illia:YNnPzj1l0R7xb8GI@cluster0-dcvzy.mongodb.net/shop`
 
 const app = express()
 
 const hbs = exphbs.create({
     defaultLayout: 'main',
     extname: 'hbs'
+})
+
+const store = new MongoStore({
+    collection: 'sessions',
+    uri: MONGODB_URL
 })
 
 app.engine('hbs', hbs.engine)
@@ -28,7 +38,8 @@ app.use(express.urlencoded({ extended: true }))
 app.use(session({
     secret: 'some secret value',
     resave: false,
-    saveUninitialized: false
+    saveUninitialized: false,
+    store
 }))
 app.use(varMiddleware)
 
@@ -45,23 +56,11 @@ const PORT = process.env.PORT || 5000
 // Running app
 async function start() {
     try {
-        const url = `mongodb+srv://illia:YNnPzj1l0R7xb8GI@cluster0-dcvzy.mongodb.net/shop`
-        await mongoose.connect(url, {
+        await mongoose.connect(MONGODB_URL, {
             useNewUrlParser: true,
             useUnifiedTopology: true,
             useFindAndModify: false
         })
-
-        // const candidate = await User.findOne()
-        //
-        // if (!candidate) {
-        //     const user = new User({
-        //         email: 'shabelnyk.ilya@mail.ru',
-        //         name: 'Illia',
-        //         cart: { items: [] }
-        //     })
-        //     await user.save()
-        // }
 
         app.listen(PORT, () => {
             console.log(`Server is running on port ${PORT}`)
