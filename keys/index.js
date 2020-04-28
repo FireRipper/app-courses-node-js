9ab55bbfff271e1f0b93c7463231642fb9294772
Illia <shabelnyk.ilya@gmail.com>
2020-04-29 00:44:27 +0300

    Update index.js

--- keys/index.js
+++ keys/index.js
@@ -2,7 +2,6 @@ module.exports = {
     MONGODB_URI: `mongodb+srv://illia:YNnPzj1l0R7xb8GI@cluster0-dcvzy.mongodb.net/shop`,
     PORT: 5000,
     SESSION_SECRET: 'some secret value',
-    SENDGRID_API_KEY: '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',
     EMAIL_FROM: 'shabelnik.ilya@mail.ru',
     BASE_URL: 'http://localhost:5000'
 }
